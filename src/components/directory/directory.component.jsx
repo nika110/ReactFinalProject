b9def16342d22c30a720745f059060a4a4875e7f
nika110 <nika.asatiani.1@btu.edu.ge>
2020-12-16 05:19:51 +0400

    Building ShopPage

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,29 +13,34 @@ class Directory extends React.Component {
         {
           title: 'hats',
           imageUrl: 'https://media.gq.com/photos/5a04f9a398002d2e253679f5/master/pass/fall-hats-gq-style-0816-01-1.jpg',
-          id: 1
+          id: 1,
+          linkUrl: 'hats'
         },
         {
           title: 'jackets',
           imageUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rw-waterproof-jackets-1597863491.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*',
-          id: 2
+          id: 2,
+          linkUrl: ''
         },
         {
           title: 'sneakers',
           imageUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/092719-sneakers-1569598764.png?crop=1.00xw:1.00xh;0,0&resize=1200:*',
-          id: 3
+          id: 3,
+          linkUrl: ''
         },
         {
           title: 'womens',
           imageUrl: 'https://s.hdnux.com/photos/01/12/67/36/19625870/6/rawImage.jpg',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: ''
         },
         {
           title: 'mens',
           imageUrl: 'https://www.themodestman.com/wp-content/uploads/2020/02/Best-mens-spring-jackets.jpg',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: ''
         }
       ]
     };
@@ -44,8 +49,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
